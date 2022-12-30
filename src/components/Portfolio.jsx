8f8d2b3e6fdd9fd3bac3dafc541534f0b996b87d
Supratim Malakar <supratimmalakar5@gmail.com>
2022-12-30 22:22:04 +0530

    Add new projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -19,9 +19,78 @@ import triomics3 from "../assets/triomics3.jpg";
 import create1 from "../assets/create1.png"
 import create2 from "../assets/create2.png";
 import create3 from "../assets/create3.png";
+import create4 from "../assets/create4.png";
+import create5 from "../assets/create5.png";
 import createlogo from "../assets/createlogo.png"
+import spotify2yt1 from '../assets/spotify2yt1.png'
+import spotify2yt2 from "../assets/spotify2yt2.png";
+import spotify2yt4 from "../assets/spotify2yt4.png";
+import blogapp1 from "../assets/blogapp1.png";
+import blogapp2 from "../assets/blogapp2.png";
+import blogapp3 from "../assets/blogapp3.png";
+import blogapp4 from "../assets/blogapp4.png";
 
 export const projects = [
+  {
+    title: "Blogger",
+    tags: [
+      "NextJS",
+      "NodeJS",
+      "JWT",
+      "Redux",
+      "Markdown",
+      "MongoDB",
+      "Express",
+      "TailwindCSS",
+    ],
+    mainImg: blogapp4,
+    hoverImg: blogapp1,
+    id: 6,
+    shortDesc:
+      "I've created this Markdown powered blogging social media platform when people can follow each other and stay updated on their blogs",
+    description:
+      "On the frontend side, I've used Next.js for server-side rendering and path based routing. On the server side, I've used Node.js, Express and MongoDB. The authentication is stateless using JWT token. \n The text editor is a rich text editor that uses Markdown letting the user customize the content and even add images to their blogs.",
+    technicals: [
+      "Stateless Authentication",
+      "Cookies",
+      "MongoDB CRUD operations",
+      "Redux",
+      "Markdown",
+      "MongoDB",
+      "Express",
+    ],
+    images: [blogapp4, blogapp1, blogapp2, blogapp3],
+    appLink: "https://blogapp-supratimmalakar.vercel.app",
+    repoLink: "https://github.com/supratimmalakar/blogapp",
+  },
+  {
+    title: "Spotify2YT",
+    tags: [
+      "ReactJS",
+      "NodeJS",
+      "Webpack",
+      "Google API",
+      "Spotify API",
+      "Chrome Identity",
+    ],
+    mainImg: spotify2yt4,
+    hoverImg: spotify2yt1,
+    id: 5,
+    shortDesc:
+      "A Chrome extension that lets users convert their Spotify playlists to Youtube playlists.",
+    description:
+      "Spotify2YT allows users to easily convert their Spotify playlists into YouTube playlists. Simply install the extension and log in to both your Spotify and YouTube accounts. Then, select a Spotify playlist that you want to convert and our extension will automatically search for and add the corresponding songs on YouTube to a new playlist. This is a great tool for those who prefer to use YouTube to listen to music, but still want to keep their favorite Spotify playlists. \n It is built using ReactJS and webpack for bundling on the frontend and I've used NodeJS and Express in the backend.",
+    technicals: [
+      "ReactJS",
+      "NodeJS",
+      "Webpack",
+      "Google API",
+      "Spotify API",
+      "Chrome Identity",
+    ],
+    images: [spotify2yt4, spotify2yt2, spotify2yt1],
+    repoLink: "https://github.com/Sickaada/spotify2yt",
+  },
   {
     title: "Triomics",
     tags: ["ReactJS", "Redux", "MaterialUI", "Typescript"],
@@ -77,7 +146,7 @@ export const projects = [
     description:
       "Worked as Frontend Developer, created Decentralized apps based on Web3 principles that connect to the blockchain withsmart contracts.\n Built components for a NFT marketplace that connects to IPFS, called smart contracts and connected with rest APIs",
     technicals: ["NextJS", "Redux", "TailwindCSS", "Typescript", "Ether"],
-    images: [create1, create2, create3],
+    images: [create1, create4, create5, create2, create3],
   },
 ];
 
